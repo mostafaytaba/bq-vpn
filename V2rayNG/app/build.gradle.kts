a8dc4ab419f0432671626f09270a0f4c319cc85e
mostafaytaba <mostafaytaba@gmail.com>
2025-09-29 18:39:28 +0330

    change version code

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.v2ray.ang"
         minSdk = 21
         targetSdk = 35
-        versionCode = 673
-        versionName = "1.10.23"
+        versionCode = 1
+        versionName = "0.0.1"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
